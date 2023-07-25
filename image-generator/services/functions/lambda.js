ad88e1d19cc51b1f1399f0d73ae7b5d82469c749
Mohammad Afzal <mohammadafzal@Mohammads-Mac-mini.local>
2023-07-25 01:04:15 -0400

    added sst and download image logic

--- image-generator/services/functions/lambda.js
+++ image-generator/services/functions/lambda.js
@@ -1,42 +1,56 @@
 import puppeteer from "puppeteer-core";
 import chromium from "@sparticuz/chromium";
 
-const url = "https://stoic-quotes-one.vercel.app/";
+const url = "https://stoic-quotes-one.vercel.app";
+const YOUR_LOCAL_CHROMIUM_PATH =
+  "/tmp/localChromium/chromium/mac_arm-1174617/chrome-mac/Chromium.app/Contents/MacOS/Chromium";
 
 export async function handler(event) {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      message: "Hello world!",
-    }),
-  };
+  // return {
+  //   statusCode: 200,
+  //   headers: { "Content-Type": "application/json" },
+  //   body: JSON.stringify({
+  //     message: "Hello world!",
+  //   }),
+  // };
   // const height = event.queryStringParameters.height || 1440;
   // const width = event.queryStringParameters.width || 2560;
+  const height = 1080 ?? 1440;
+  const width = 1080 ?? 2560;
 
-  // const browser = await puppeteer.launch({
-  //   args: chromium.args,
-  //   defaultViewport: chromium.defaultViewport,
-  //   executablePath: await chromium.executablePath(),
-  //   headless: chromium.headless,
-  //   ignoreHTTPSErrors: true,
-  // });
+  const launchArgs = {
+    args: chromium.args,
+    defaultViewport: {
+      ...chromium.defaultViewport,
+      deviceScaleFactor: 2,
+      width: Number(width),
+      height: Number(height),
+    },
+    executablePath: process.env.IS_LOCAL
+      ? YOUR_LOCAL_CHROMIUM_PATH
+      : await chromium.executablePath(),
+    headless: true || chromium.headless,
+    ignoreHTTPSErrors: true,
+    slowMo: 200,
+  };
 
-  // const page = await browser.newPage();
+  const browser = await puppeteer.launch(launchArgs);
+
+  const page = await browser.newPage();
 
   // await page.setViewport({
   //   width: Number(width),
   //   height: Number(height),
   // });
 
-  // // Navigate to the url
-  // await page.goto(url);
+  // Navigate to the url
+  await page.goto(url);
 
-  // return {
-  //   statusCode: 200,
-  //   // Return as binary data
-  //   isBase64Encoded: true,
-  //   headers: { "Content-Type": "image/png" },
-  //   body: await page.screenshot({ encoding: "base64" }),
-  // };
+  return {
+    statusCode: 200,
+    // Return as binary data
+    isBase64Encoded: true,
+    headers: { "Content-Type": "image/png" },
+    body: await page.screenshot({ encoding: "base64" }),
+  };
 }
--- image-generator/stacks/MyStack.js
+++ image-generator/stacks/MyStack.js
@@ -1,6 +1,9 @@
 import { Api } from "@serverless-stack/resources";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
+const layerArn = "arn:aws:lambda:us-east-1:824714483059:layer:chromium_v109:1";
+// const layerArn = "arn:aws:lambda:us-east-1:824714483059:layer:chromium_v114:1";
+
 export function MyStack({ stack }) {
   // const layer = new lambda.LayerVersion(stack, "chromium", {
   //   code: lambda.Code.fromAsset("layers/chromium.zip"),
@@ -14,9 +17,15 @@ export function MyStack({ stack }) {
           // Increase the timeout for generating screenshots
           timeout: 15,
           // Load Chrome in a Layer
-          // layers: [layer],
+          layers: [
+            lambda.LayerVersion.fromLayerVersionArn(
+              stack,
+              "ChromeLayer",
+              layerArn
+            ),
+          ],
           // Exclude bundling it in the Lambda function
-          // bundle: { externalModules: ["chrome-aws-lambda"] },
+          bundle: { externalModules: ["@sparticuz/chromium"] },
         },
       },
     },
